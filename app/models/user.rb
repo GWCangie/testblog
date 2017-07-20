49559457ee6e406bba14961c059e7c81e988404a
Angela <angeladon22@gmail.com>
2017-07-20 15:57:29 -0400

    updates missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+      attr_accessor :email, :password, :password_confirmation, :remember_me, :user_name
+
+
+def will_save_change_to_email?
+end
 end
