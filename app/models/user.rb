7cd9e5ff4986343bfdab00237d457bfbc5977f4e
Angela <angeladon22@gmail.com>
2017-07-20 14:19:59 -0400

    adding email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable,:validatable, :confirmable
+   attr_accessor :email, :password, :password_confirmation, :remember_me, :user_name
+       end
+
+def will_save_change_to_email?
 end
