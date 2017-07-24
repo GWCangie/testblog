53bced41b7c875a81a41941519c469e2b74ab1d1
Angela <angeladon22@gmail.com>
2017-07-24 14:27:04 -0400

    modifications to applications.yml

--- db/migrate/20170721151956_add_username_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration[5.1]
-  def change
-    #added create_table :users
-    #create_table :users
-    #add_column :user, :username, :string
-    add_index :user, :username, unique: true
-  end
-end
