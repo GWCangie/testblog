0436e11b1528cf69a07f0c1f27c4e505b8b85c20
Angela <angeladon22@gmail.com>
2017-07-24 11:03:37 -0400

    modified gitignore, application controller and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
          :recoverable, :rememberable, :trackable,:validatable, :confirmable
    attr_accessor :email, :password, :password_confirmation, :remember_me, :user_name
-       end
+  end
 
-def will_save_change_to_email?
-end
-=======
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
-
-      attr_accessor :email, :password, :password_confirmation, :remember_me, :username
 
 
   def will_save_change_to_email?
   end
 
 
-end
->>>>>>> sengrindtesting
